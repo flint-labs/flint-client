32c9085cf4c97da5af82020d6d8849f342839263
wallypark710 <wallypark710@gmail.com>
2019-02-26 13:57:18 +0900

    Edit navigation structure
     - 바텀탭을 기본으로하는 화면 구조로 변경.
     - 각각의 탭에서 새로운 스택구조를 형성하도록 구조를 변경.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { View, Text, ScrollView, Dimensions } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator } from 'react-navigation';
-
-import Referee from '../Referee';
-import Dashboard from '../Dashboard';
-import History from '../History';
-import UserInfo from '../UserInfo';
+import { View, ScrollView, Dimensions } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
 import Intro from './Intro';
 import Feedback from './Feedback';
@@ -24,81 +18,11 @@ const Home = () => (
   </View>
 );
 
-export default createBottomTabNavigator(
-  {
-    Home: {
-      screen: Home,
-      navigationOptions: {
-        tabBarLable: 'HOME',
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="ios-cafe" color={tintColor} size={24} />
-        ),
-        header: {
-          visible: false,
-        },
-      },
-    },
-    Referee: {
-      screen: Referee,
-      navigationOptions: {
-        tabBarLable: 'Referee',
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="ios-cafe" color={tintColor} size={24} />
-        ),
-        header: {
-          visible: false,
-        },
-      },
-    },
-    Dashboard: {
-      screen: Dashboard,
-      navigationOptions: {
-        tabBarLable: 'Dashboard',
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="ios-cafe" color={tintColor} size={24} />
-        ),
-        header: {
-          visible: false,
-        },
-      },
-    },
-    History: {
-      screen: History,
-      navigationOptions: {
-        tabBarLable: 'History',
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="ios-cafe" color={tintColor} size={24} />
-        ),
-        header: {
-          visible: false,
-        },
-      },
-    },
-    UserInfo: {
-      screen: UserInfo,
-      navigationOptions: {
-        tabBarLable: 'UserInfo',
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="ios-cafe" color={tintColor} size={24} />
-        ),
-        header: {
-          visible: false,
-        },
-      },
+export default createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      headerTitle: 'Flint',
     },
   },
-  {
-    tabBarOptions: {
-      activeTintColor: 'black',
-      inactiveTintColor: 'gray',
-      style: {
-        backgroundColor: 'white',
-        borderTopWidth: 0,
-        shadowOffset: { width: 5, height: 3 },
-        shadowColor: 'black',
-        shadowOpacity: 0.5,
-        elevation: 5,
-      },
-    },
-  },
-);
+});
