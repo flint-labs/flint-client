efc0b6c73111755430e559b2fcc067e05d4fd280
rjwu95 <yugun000@gmail.com>
2019-02-26 14:10:22 +0900

    Merge modified navigator
     - Merge and resolve conflict

--- App.js
+++ App.js
@@ -1,17 +1,68 @@
 import React from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Home from './src/screens/Home';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const RootStack = createStackNavigator(
+import Home from './src/screens/Home';
+import Referee from './src/screens/Referee';
+import Dashboard from './src/screens/Dashboard';
+import History from './src/screens/History';
+import UserInfo from './src/screens/UserInfo';
+
+const Root = createBottomTabNavigator(
   {
-    Home,
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLable: 'HOME',
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-cafe" color={tintColor} size={24} />,
+      },
+    },
+    Referee: {
+      screen: Referee,
+      navigationOptions: {
+        tabBarLable: 'Referee',
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-cafe" color={tintColor} size={24} />,
+      },
+    },
+    Dashboard: {
+      screen: Dashboard,
+      navigationOptions: {
+        tabBarLable: 'Dashboard',
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-cafe" color={tintColor} size={24} />,
+      },
+    },
+    History: {
+      screen: History,
+      navigationOptions: {
+        tabBarLable: 'History',
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-cafe" color={tintColor} size={24} />,
+      },
+    },
+    UserInfo: {
+      screen: UserInfo,
+      navigationOptions: {
+        tabBarLable: 'UserInfo',
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-cafe" color={tintColor} size={24} />,
+      },
+    },
   },
   {
-    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+      style: {
+        backgroundColor: 'white',
+        borderTopWidth: 0,
+        shadowOffset: { width: 5, height: 3 },
+        shadowColor: 'black',
+        shadowOpacity: 0.5,
+        elevation: 5,
+      },
+    },
   },
 );
 
-const AppContainer = createAppContainer(RootStack);
+const AppContainer = createAppContainer(Root);
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
@@ -15,3 +16,27 @@ class component extends React.Component {
 }
 
 export default component;
+=======
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Dashboard </Text>
+      </View>
+    );
+  }
+}
+
+export default Dashboard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+>>>>>>> 070038ce0b9fea0fc5ce5bf6c771564cf9a816e0
