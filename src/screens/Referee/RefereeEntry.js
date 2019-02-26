06063819c2763e6b9a61af4b6713f1d6bdc20d8e
wallypark710 <wallypark710@gmail.com>
2019-02-26 20:56:03 +0900

    Add onPress event on RefereeEntry
     - Onpress event 기본 틀 작성

--- src/screens/Referee/RefereeEntry.js
+++ src/screens/Referee/RefereeEntry.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 
 import styles from './Styles';
@@ -7,24 +7,26 @@ import styles from './Styles';
 const fakeImage = require('../../../assets/images/Home/cate1.jpg');
 
 const RefereeEntry = ({ data }) => (
-  <View style={styles.refereeEntry}>
-    <View style={styles.imageContainer}>
-      <Image
-        source={fakeImage}
-        style={{
-          width: 120,
-          resizeMode: 'cover',
-          flex: 1,
-          borderRadius: 5,
-        }}
-      />
+  <TouchableOpacity onPress={() => console.log('click Referee entry')}>
+    <View style={styles.refereeEntry}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={fakeImage}
+          style={{
+            width: 120,
+            resizeMode: 'cover',
+            flex: 1,
+            borderRadius: 5,
+          }}
+        />
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.titleText}>{data.title}</Text>
+        <Text style={styles.descriptionText}>{data.description}</Text>
+        <Text style={styles.stateText}>{data.state}</Text>
+      </View>
     </View>
-    <View style={styles.bodyContainer}>
-      <Text style={styles.titleText}>{data.title}</Text>
-      <Text style={styles.descriptionText}>{data.description}</Text>
-      <Text style={styles.stateText}>{data.state}</Text>
-    </View>
-  </View>
+  </TouchableOpacity>
 );
 
 RefereeEntry.propTypes = {
