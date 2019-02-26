19abbeb96bb012262868a59b15cdf527c218c7b2
wallypark710 <wallypark710@gmail.com>
2019-02-26 16:40:45 +0900

    Add PropsType package

--- src/screens/Referee/index.js
+++ src/screens/Referee/index.js
@@ -1,22 +1,11 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './Styles';
 
-class Referee extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text> Referee </Text>
-      </View>
-    );
-  }
-}
+const Referee = () => (
+  <View style={styles.container}>
+    <Text> Referee </Text>
+  </View>
+);
 
 export default Referee;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
