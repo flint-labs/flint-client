f3ae2c20a010dd89273d55ba300a78c6508123d3
rjwu95 <yugun000@gmail.com>
2019-03-06 22:39:55 +0900

    Modify style Doit
     - Add header on Doit

--- src/screens/Dashboard/DoIt.js
+++ src/screens/Dashboard/DoIt.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  TouchableOpacity, View, TextInput, Alert, Image, SafeAreaView,
+  TouchableOpacity, View, TextInput, Alert, Image, SafeAreaView, Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
 // import { createStackNavigator } from 'react-navigation';
@@ -60,48 +60,65 @@ class DoIt extends React.Component {
   };
 
   render() {
-    const { modalVisible } = this.props;
+    const { modalVisible, recentChallenge } = this.props;
     const { photo, text } = this.state;
     return (
       <SafeAreaView>
-        <KeyboardAwareScrollView>
-          <Modal
-            animationType="slide"
-            transparent={false}
-            visible={modalVisible}
-            onRequestClose={this.closeModal}
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={modalVisible}
+          onRequestClose={this.closeModal}
+        >
+          <KeyboardAwareScrollView
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            contentContainerStyle={{ flex: 1 }}
+            enableAutomaticScroll
+            extraHeight={180}
           >
-            <TouchableOpacity onPress={this.closeModal} style={{ flex: 0.5 }}>
-              <Icon size={35} name="ios-arrow-round-back" />
-            </TouchableOpacity>
-            <View style={{ flex: 10 }}>
-              <View style={styles.modalTextInputContainer}>
-                <TextInput
-                  style={styles.modalTextInput}
-                  placeholder={'도전에 대한 일지를 남겨주세요\n( 50자 이내 )'}
-                  multiline
-                  autoFocus
-                  blurOnSubmit
-                  onChangeText={inputText => this.setState({ text: inputText })}
-                  maxLength={50}
-                  value={text}
-                />
-              </View>
-              <View style={{ flex: 1, marginLeft: '5%' }}>
-                <TouchableOpacity style={styles.imageRefBtn} onPress={this.selectPicture}>
-                  <Image
-                    source={photo ? { uri: photo } : cameraImage}
-                    style={{ width: 80, height: 80 }}
-                  />
+            <View style={{ flex: 1 }}>
+              <View style={styles.doItHeaderContainer}>
+                <TouchableOpacity onPress={this.closeModal} style={{ flex: 0.5, height: '100%' }}>
+                  <Icon size={35} name="ios-arrow-round-back" />
                 </TouchableOpacity>
+                <View
+                  style={styles.doItHeaderTitleContainer}
+                >
+                  <Text style={{ fontSize: 20 }}>{recentChallenge.title}</Text>
+                </View>
+                <View style={{ flex: 0.5, height: '100%' }}>
+                  <Text />
+                </View>
               </View>
-              <View style={styles.submitBtnContainer}>
-                <OrangeButton text="제출" onPress={this.submitBtnHandler} />
+              <View style={{ flex: 1 }}>
+                <View style={styles.modalTextInputContainer}>
+                  <TextInput
+                    style={styles.modalTextInput}
+                    placeholder={'도전에 대한 일지를 남겨주세요\n( 50자 이내 )'}
+                    multiline
+                    autoFocus
+                    blurOnSubmit
+                    onChangeText={inputText => this.setState({ text: inputText })}
+                    maxLength={50}
+                    value={text}
+                  />
+                </View>
+                <View style={{ flex: 1, marginLeft: '5%' }}>
+                  <TouchableOpacity style={styles.imageRefBtn} onPress={this.selectPicture}>
+                    <Image
+                      source={photo ? { uri: photo } : cameraImage}
+                      style={{ width: 80, height: 80 }}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.submitBtnContainer}>
+                  <OrangeButton text="제출" onPress={this.submitBtnHandler} />
+                </View>
+                <View style={{ flex: 2 }} />
               </View>
-              <View style={{ flex: 1 }} />
             </View>
-          </Modal>
-        </KeyboardAwareScrollView>
+          </KeyboardAwareScrollView>
+        </Modal>
       </SafeAreaView>
     );
   }
