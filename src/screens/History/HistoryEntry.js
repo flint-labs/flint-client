618f55fe7a455edd97719a80e1eff438509bef32
wallypark710 <wallypark710@gmail.com>
2019-03-09 23:00:17 +0900

    Remove history entry icon
     - 히스토리 성공/실패 아이콘 삭제

--- src/screens/History/HistoryEntry.js
+++ src/screens/History/HistoryEntry.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './style';
-
-const pass = require('../../../assets/images/History/pass.png');
-const faile = require('../../../assets/images/History/faile.png');
 
 const HistoryEntry = ({ data, handlePress }) => {
   const end = new Date(data.endAt);
@@ -25,29 +22,26 @@ const HistoryEntry = ({ data, handlePress }) => {
         <View style={{ flex: 1 }}>
           {data.state === 'success' ? (
             <View style={{ flex: 1, justifyContent: 'center' }}>
-              <ImageBackground
-                source={pass}
-                style={{ flex: 1 }}
-                imageStyle={{
-                  width: 120,
-                  resizeMode: 'contain',
-                }}
-              />
               <View style={{ alignItems: 'center' }}>
-                <Text style={{ fontSize: 12, color: 'green' }}>Success</Text>
+                <Text
+                  style={{
+                    fontFamily: 'Fontrust',
+                    fontSize: 35,
+                    color: 'green',
+                  }}
+                >
+                  Success
+                </Text>
               </View>
             </View>
           ) : (
             <View style={{ flex: 1, justifyContent: 'center' }}>
-              <ImageBackground
-                source={faile}
-                style={{
-                  flex: 1,
-                }}
-                imageStyle={{ width: 110, resizeMode: 'contain' }}
-              />
               <View style={{ alignItems: 'center' }}>
-                <Text style={{ fontSize: 12, color: 'red' }}>Failure</Text>
+                <Text
+                  style={{ fontFamily: 'Fontrust', fontSize: 35, color: 'red' }}
+                >
+                  Failure
+                </Text>
               </View>
             </View>
           )}
