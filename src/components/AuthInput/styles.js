abc2177c56227f05a2214e73a435bc328407650d
sunjae-kim <kimsj9484@gmail.com>
2019-03-15 14:56:16 +0900

    Increase height of Authinput component
     - Add padding top to TextInput

--- src/components/AuthInput/styles.js
+++ src/components/AuthInput/styles.js
@@ -6,16 +6,18 @@ export default StyleSheet.create({
   input: {
     flexDirection: 'row',
     width: width * 0.7,
-    marginTop: 20,
+    marginTop: 10,
     paddingTop: 10,
     paddingBottom: 2,
     borderBottomWidth: 1,
     borderBottomColor: '#333',
     marginBottom: 10,
+    alignItems: 'flex-end',
   },
   inputElement: {
     fontSize: 17,
     width: width * 0.55,
+    paddingTop: 20,
     paddingLeft: 10,
   },
 });
